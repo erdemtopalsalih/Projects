cee27c22cfb8c7ff8c00401954e32d84546de183
Erdem Topalsalih <96544790+erdemtopalsalih@users.noreply.github.com>
2023-09-02 12:46:24 +0300

    Delete MsgHello directory

--- MsgHello/MsgHello/main.c
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine, int nShowCmd)
-{
-	MessageBox(NULL, TEXT("Hello, Windows 11!"), TEXT("MsgHello"), 0);
-	return 0;
-}
